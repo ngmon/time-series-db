20ba498640f76aec2b9084ccb421db527bc4f49f
Michal <Michal@Michal-ntb>
2012-11-12 13:46:25 +0100

    mysql, postgre added

--- src/main/java/org/monitoring/caliperBenchmark/CaliperDBStrategy.java
+++ src/main/java/org/monitoring/caliperBenchmark/CaliperDBStrategy.java
@@ -8,17 +8,20 @@ import com.google.caliper.Runner;
 import com.google.caliper.SimpleBenchmark;
 import java.util.Locale;
 import org.monitoring.db.Database;
-import org.monitoring.db.DerbyDatabase;
 import org.monitoring.db.MongoDatabase;
+import org.monitoring.db.MySQLDatabase;
+import org.monitoring.db.PostgreSQLDatabase;
 
 /**
  *
  * @author Michal
+ * results online
+ *   http://microbenchmarks.appspot.com/run/michal.dubravcik@gmail.com/org.monitoring.caliperBenchmark.CaliperDBStrategy
  */
 public class CaliperDBStrategy extends SimpleBenchmark {
 
     Database mongo = new MongoDatabase();
-    Database derby = new DerbyDatabase();
+    Database postgre = new PostgreSQLDatabase();
     Documents documents = new Documents();
 
     @Override
@@ -28,7 +31,7 @@ public class CaliperDBStrategy extends SimpleBenchmark {
     @Override
     public void tearDown(){
         mongo.tearDown();
-        derby.tearDown();
+        postgre.tearDown();
     }
 
     public void timeMongoStrategy(int reps) {
@@ -37,9 +40,9 @@ public class CaliperDBStrategy extends SimpleBenchmark {
         }
     }
 
-    public void timeDerbyStrategy(int reps) {
+    public void timePostgreSQLStrategy(int reps) {
         for (int i = 0; i < reps; i++) {            
-            derby.saveDocument(documents.getDocument(i));
+            postgre.saveDocument(documents.getDocument(i));
         }
     }
 
---
+++ src/main/java/org/monitoring/db/PostgreSQLDatabase.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.monitoring.db;
+
+import com.mongodb.DBObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Michal
+ */
+public class PostgreSQLDatabase implements Database {
+
+    Connection conn = null;
+    String query = null;
+    PreparedStatement st = null;
+
+    public PostgreSQLDatabase() {
+        try {
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "root");
+            query = "INSERT INTO test1 (occurrenceTime,detectionTime,hostname,type,"
+                    + "application,process,processId,severity,priority,value)VALUES(?,?,?,?,?,?,?,?,?,?)";
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void saveDocument(DBObject object) {
+        try {
+            st = conn.prepareStatement(query);
+            st.setString(1, (String) object.get("occurrenceTime"));
+            st.setString(2, (String) object.get("detectionTime"));
+            st.setString(3, (String) object.get("hostname"));
+            st.setString(4, (String) object.get("type"));
+            st.setString(5, (String) object.get("application"));
+            st.setString(6, (String) object.get("process"));
+            st.setString(7, (String) object.get("processId"));
+            st.setInt(8, Integer.valueOf(object.get("severity").toString()));
+            st.setInt(9, Integer.valueOf(object.get("priority").toString()));
+            st.setString(10, "JSON can't have . (dot) in key string"); //(String) event.get("http://collectd.org/5.1/events.jsch").toString());
+
+            int out = st.executeUpdate();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void tearDown() {
+        PreparedStatement drop;
+        try {
+            drop = conn.prepareStatement("DELETE FROM test1");
+            drop.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
